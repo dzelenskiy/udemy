751e9be64560c37f2af22e177f056003b0fbc578
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-02-09 17:02:30 -0600

    Completed Section 7, Lecture 92

--- react-the-complete-guide/react-complete-guide/src/containers/App.js
+++ react-the-complete-guide/react-complete-guide/src/containers/App.js
@@ -40,6 +40,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount');  
   }
 
+  shouldComponenUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }  
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   nameChangedHandler = ( newName, id ) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
