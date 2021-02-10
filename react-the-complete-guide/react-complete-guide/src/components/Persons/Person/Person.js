5e0f1eaf397a1805ed10232ea98e863d02ada7e9
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-02-10 14:40:37 -0600

    Completed Section 7, Lecture 108

--- react-the-complete-guide/react-complete-guide/src/components/Persons/Person/Person.js
+++ react-the-complete-guide/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import withClass from '../../../hoc/withClass';
 import classes from './Person.css'
 
 class Person extends Component {
@@ -25,15 +26,15 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <React.Fragment>
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </React.Fragment>
         )
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
---
+++ react-the-complete-guide/react-complete-guide/src/hoc/Auxil.js
@@ -0,0 +1,5 @@
+//Aux is a reserved word in Windows, using Auxil for compatibility
+
+const auxil = props => props.children;
+
+export default auxil;
