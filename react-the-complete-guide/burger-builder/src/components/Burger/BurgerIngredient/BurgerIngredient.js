9c4cb6a854aedb297d3e04ab507969ed0b4e4dba
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-02-11 15:16:17 -0600

    Middle of Section 8, Lecture 126

---
+++ react-the-complete-guide/burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from 'BurgerIngredient.module.css';
+
+const burgerIngredient = props => {
+
+    let ingredient = null;
+
+    switch(props.type) {
+        case('bread-bottom'):
+            ingredient = <div className={classes.BreadBottom}></div>;
+            break;
+        case('bread-top'):
+            ingredient = (
+                <div className={classes.BreadTop}>
+
+                </div>
+            );
+
+    }
+
+};
+
+export default burgerIngredient;
