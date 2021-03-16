4aa7f85a771026454742aa820191b1978191f1c4
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-03-16 10:18:55 -0500

    Completed Section 11, Lecture 217

--- react-the-complete-guide/routing--react-suspense-start/src/App.js
+++ react-the-complete-guide/routing--react-suspense-start/src/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
-
-import Posts from './containers/Posts';
 import User from './containers/User';
 import Welcome from './containers/Welcome';
 
+// import Posts from './containers/Posts';
+const Posts = React.lazy(() => import('./containers/Posts'));
+
+
 class App extends Component {
+
+  // state = {show: false};
+
+  // modeHandler = () => {
+  //   this.setState(prevState => {
+  //       return {show: !prevState.show};
+  //   });
+  // };
+
   render() {
     return (
+      // <>
+      // <button onClick={this.modeHandler}>Toggle Mode</button>
+      // {this.state.show 
+      //   ? 
+      //   <Suspense fallback={<div>Loading...</div>}>
+      //     <Posts />
+      //   </Suspense>
+      //   :
+      //   <User />
+      // }
+      // </>
       <BrowserRouter>
         <React.Fragment>
           <nav>
@@ -16,7 +38,15 @@ class App extends Component {
           </nav>
           <Route path="/" component={Welcome} exact />
           <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
+          {/* <Route path="/posts" component={Posts} /> */}
+          <Route 
+            path="/posts" 
+            render={() => (
+              <Suspense fallback={<div>Loading...</div>}>
+                <Posts />
+              </Suspense>
+            )} 
+          />
         </React.Fragment>
       </BrowserRouter>
     );
