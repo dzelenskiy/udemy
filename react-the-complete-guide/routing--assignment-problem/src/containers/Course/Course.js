6eecd94c16ae853ce20c229b297fbf5e4a7630cc
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-03-16 15:39:44 -0500

    Completed Section 11, Assignment 3

--- react-the-complete-guide/routing--assignment-problem/src/containers/Course/Course.js
+++ react-the-complete-guide/routing--assignment-problem/src/containers/Course/Course.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
+
+        parseTitleFromQueryString = () => {
+            const queryString = this.props.location.search;
+            const params = new URLSearchParams(queryString);
+            let title = params.get('title');
+            !title && (title = 'Missing Title');
+            return title;
+        }
+
     render () {
+        console.log(this.props);
+
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{this.parseTitleFromQueryString()}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
