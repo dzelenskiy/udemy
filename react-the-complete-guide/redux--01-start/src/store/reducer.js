a56e28e36a12daf9211fc2f466e2acb84819216f
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-03-19 15:26:50 -0500

    Completed Section 14, Lecture 268

--- react-the-complete-guide/redux--01-start/src/store/reducer.js
+++ react-the-complete-guide/redux--01-start/src/store/reducer.js
@@ -1,14 +1,48 @@
-const initialState = {
-    counter: 0
-}
+// import * as actionTypes from './actions';
 
-const reducer = (state = initialState, action) => {
-    if(action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    return state;
-}
+// const initialState = {
+//     counter: 0,
+//     results: []
+// }
 
-export default reducer;
+// const reducer = (state = initialState, action) => {
+
+//     switch(action.type) {
+//         case actionTypes.INCREMENT:
+//             return {
+//                 ...state,
+//                 counter: state.counter + 1
+//             };
+//         case actionTypes.DECREMENT:
+//             return {
+//                 ...state,
+//                 counter: state.counter - 1
+//             };
+//         case actionTypes.ADD:
+//             return {
+//                 ...state,
+//                 counter: state.counter + action.value
+//             };
+//         case actionTypes.SUBTRACT:
+//             return {
+//                 ...state,
+//                 counter: state.counter - action.value
+//             };
+//         case actionTypes.STORE_RESULT:
+//             return {
+//                 ...state,
+//                 // using concat b/c it returns a new arrray therefore updating immutably
+//                 results: state.results.concat({id: new Date(), value: state.counter})
+//             };
+//         case actionTypes.DELETE_RESULT:
+//             return {
+//                 ...state,
+//                 //using filter here b/c it returns a new arrray therefore updating immutably
+//                 results: state.results.filter(result => result.id !== action.resultElId)
+//             };
+//         default:
+//             return state;
+//     }
+// }
+
+// export default reducer;
