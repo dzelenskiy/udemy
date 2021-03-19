e7639efad75b96dee317010e083d581ef7ccdb4a
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-03-19 11:55:13 -0500

    Completed Section 14, Lecture 259

--- react-the-complete-guide/redux--01-start/redux-basics.js
+++ react-the-complete-guide/redux--01-start/redux-basics.js
@@ -1,19 +1,58 @@
-const redux = require('redux');
-const createStore = redux.createStore;
+// this is node js only example, w/o react
+// can be run by running "node <filename>" in terminal
 
+// summary...
+// STORE:  stores state information 
+// SUBSCRIPTION:  informs when state has been updated
+// DISPATCH:  sends commands w parameters (aka actions) 
+// REDUCER:  defines logic to be executed for the commands/actions
+
+// importing redux w node.js syntax for imports
+const redux = require('redux');
+
+
+// Reducer ================================================================================
 const initialState = {
     counter: 0
 };
-
-// Reducer
-const rootReducer = (state = initialState, Action) => {
+// created first b/c its closely linked to Store and used in Store creation
+// need to initialize state here, otherwise will be undefined
+const rootReducer = (state = initialState, action) => {
+    if(action.type === 'INC_COUNTER') {
+        // state needs to always be updated immutably
+        // copy state first, then update copy and set it as state
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+    if(action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state;
 };
 
-// Store
+
+// Store ===================================================================================
+const createStore = redux.createStore;
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-// Dispatching Action
 
-// Subsciption
+// Subsciption =============================================================================
+// subsciption is to inform when state is updated
+// subscribe method takes function as param which will execute when state is updated
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
+
+// Dispatching Action ======================================================================
+// 1st param type is expected, conventionally all caps and unique 
+store.dispatch({type: 'INC_COUNTER'});
+// 2nd+ params optional, named anything, can be put into one js obj typically called payload
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
--- react-the-complete-guide/redux--01-start/src/containers/Counter/Counter.js
+++ react-the-complete-guide/redux--01-start/src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -28,7 +29,7 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +39,13 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+// comes between class/function and export
+// maps state managed by redux (passed in as a param) to this components props
+// name of function can be anything but is typically mapStateToProps
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    };
+}
+
+export default connect(mapStateToProps)(Counter);
