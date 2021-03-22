7054fb8a156a9e0f070f2e20c48ad710d30d6102
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-03-22 16:05:13 -0500

    Completed Section 16, Lecture 288

--- react-the-complete-guide/redux--01-start/src/containers/Counter/Counter.js
+++ react-the-complete-guide/redux--01-start/src/containers/Counter/Counter.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-
+import { connect } from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
+// import * as actionTypes from '../../store/actions/actions';
+import * as actionCreators from '../../store/actions/actions';
+
 
 class Counter extends Component {
 
@@ -39,12 +40,18 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-        onAddCounter: () => dispatch({type: actionTypes.ADD, value: 5}),
-        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 5}),
-        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
-        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
+        // onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        // onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
+        // onAddCounter: () => dispatch({type: actionTypes.ADD, value: 5}),
+        onAddCounter: () => dispatch(actionCreators.add(5)),
+        // onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 5}),
+        onSubtractCounter: () => dispatch(actionCreators.subtract(5)),
+        // onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
+        onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+        // onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
+        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
     };
 };
 
