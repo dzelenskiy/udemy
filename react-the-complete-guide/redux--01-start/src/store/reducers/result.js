0166492921263d8c7d972370db305ebaa64ae380
Dmitriy Zelenskiy <d.zelenskiy@gmail.com>
2021-03-24 08:37:30 -0500

    Completed Section 16, Lecture 293

--- react-the-complete-guide/redux--01-start/src/store/reducers/result.js
+++ react-the-complete-guide/redux--01-start/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -8,17 +9,20 @@ const reducer = (state = initialState, action) => {
 
     switch(action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                // using concat b/c it returns a new arrray therefore updating immutably
-                results: state.results.concat({id: new Date(), value: action.result})
-            };
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
+            // return {
+            //     ...state,
+            //     // using concat b/c it returns a new arrray therefore updating immutably
+            //     results: state.results.concat({id: new Date(), value: action.result})
+            // };
         case actionTypes.DELETE_RESULT:
-            return {
-                ...state,
-                //using filter here b/c it returns a new arrray therefore updating immutably
-                results: state.results.filter(result => result.id !== action.resultElId)
-            };
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId)
+            return updateObject(state, { results: updatedArray });
+            // return {
+            //     ...state,
+            //     //using filter here b/c it returns a new arrray therefore updating immutably
+            //     results: state.results.filter(result => result.id !== action.resultElId)
+            // };
         default:
             return state;
     }
